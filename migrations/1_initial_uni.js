6c75ed604b3dcaac15477037b31f661db240c089
tongyuhu <1449807759@qq.com>
2021-04-07 16:12:22 +0800

    truffle manage

---
+++ migrations/1_initial_uni.js
@@ -0,0 +1,5 @@
+const Uni = artifacts.require("Uni");
+
+module.exports = function (deployer) {
+  deployer.deploy(Uni,'0x1d4cc1a851adb750c40404718061de23577ab355','0x1d4cc1a851adb750c40404718061de23577ab355',1617784056);
+};
